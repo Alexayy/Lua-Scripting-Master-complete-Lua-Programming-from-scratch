deb2ef882c2e9b4a5264cc2b7c13f10ebd63e6b7
Aleksa Cakic <aleksa.cakic@gmail.com>
2020-12-01 02:56:16 +0100

    initial commit <3

---
+++ 4. Variables and expressions/myscript.lua
@@ -0,0 +1,19 @@
+print("hello world")
+
+print (12 * 45)
+
+print("Thank you, good bye!")
+
+bank_balance = -30.35
+
+print(bank_balance)
+
+name = "Aleksa"
+
+test_table = {1, 2, 3}
+
+print(type(bank_balance))
+print(type(name))
+print(type(test_table))
+
+print("Joining something "..bank_balance)
