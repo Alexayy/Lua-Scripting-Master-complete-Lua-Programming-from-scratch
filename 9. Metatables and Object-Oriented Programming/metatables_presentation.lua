437046f68b203250756ec219c3762fe43859d9cb
Aleksa Cakic <aleksa.cakic@gmail.com>
2020-12-08 13:16:39 +0100

    updates and moving to C

--- 9. Metatables and Object-Oriented Programming/metatables_presentation.lua
+++ 9. Metatables and Object-Oriented Programming/metatables_presentation.lua
@@ -21,6 +21,11 @@ function vector3d.add(v1, v2)
 end
 meta.__add = vector3d.add
 
+function vector3d.sub(v1, v2)
+    return vector3d.new(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
+end
+meta.__sub = vector3d.sub
+
 function vector3d.tostring(v)
     return "(".. v.x ..", ".. v.y ..", ".. v.z ..")"
 end
@@ -30,10 +35,10 @@ meta.__tostring = vector3d.tostring
 --- Create two vector3d tables
 -------------------------------------------------
 position = vector3d.new(5.0, 5.0, 5.0)
-
 velocity = vector3d.new(10.0, -3.5, 0.0)
 
-local result = velocity + position
-print(position)
-print(velocity)
-print(result)
+local result_add = velocity + position
+local result_sub = velocity - position
+
+print("Addition is: " .. tostring(result_add))
+print("Addition is: " .. tostring(result_sub))
